3a0e2be0a56ee11ee85c2842c4bf9998834d7f52
Milad <milad.beigi0098@gmail.com>
2022-10-31 12:02:30 +0330

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Container } from "react-bootstrap";
 import { Header } from "./Components/Header/Header";
 import { Footer } from "./Components/Footer/Footer";
 import { Home } from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 export const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home/>
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
       <Row>
         {products.map((item) => {
           return (
-            <Col sm={12} md={6} lg={4}>
+            <Col key={item._id} sm={12} md={6} lg={4}>
               <h3>
                 <Product product={item}/>
               </h3>
