0224e6efec741f6098097968f5a2b7662210f260
Milad <milad.beigi0098@gmail.com>
2022-10-31 09:47:30 +0330

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import {Container} from 'react-bootstrap'
+import { Header } from "./Components/Header/Header";
+import { Footer } from "./Components/Footer/Footer";
 export const App = () => {
   return (
-    <div>Heloo word</div>
-  )
-}
+    <div>
+      <Header />
+      <Container>
+        Fc Market
+      </Container>
+      <Footer />
+    </div>
+  );
+};
